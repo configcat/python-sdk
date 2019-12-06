e9d1e26ef2a7c96300beac0cc4f0e2b7983e7f03
kp-cat <52385411+kp-cat@users.noreply.github.com>
2019-12-07 00:19:16 +0100

    Revert "update package description"
    This reverts commit 29f8699b60bc67ad440f8ff809d4174d9bb31d6e.

--- setup.py
+++ setup.py
@@ -19,11 +19,8 @@ setup(
     author='ConfigCat',
     author_email='developer@configcat.com',
     description='ConfigCat SDK for Python. https://configcat.com',
-    long_description='Feature Flags created by developers for developers with ❤️. ConfigCat lets you manage '
-                     'feature flags across frontend, backend, mobile, and desktop apps without (re)deploying code. '
-                     '% rollouts, user targeting, segmentation. Feature toggle SDKs for all main languages. '
-                     'Alternative to LaunchDarkly. '
-                     'Host yourself, or use the hosted management app at https://configcat.com.',
+    long_description='ConfigCat is a configuration as a service that lets you manage your features and configurations '
+                     'without actually deploying new code.',
     install_requires=requirements,
     classifiers=[
         'Intended Audience :: Developers',
