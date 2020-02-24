acad4d7c829c55823caaf468b92d9c898289995c
sige <sige@configcat.com>
2020-02-24 13:01:53 +0100

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
     return [line for line in lines if line]
 
 
-configcatclient_version = "2.2.2"
+configcatclient_version = "3.0.1"
 
 requirements = parse_requirements('requirements.txt')
 
