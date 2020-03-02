8aec4996e350a3fb8711025c9f847129a620c8d6
kp-cat <52385411+kp-cat@users.noreply.github.com>
2020-03-03 00:45:12 +0100

    Fix webappsample

--- samples/webappsample/webapp/urls.py
+++ samples/webappsample/webapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('index1', views.index1, name='index1'),
     path('index2', views.index2, name='index2'),
     path('index3a', views.index3a, name='index3a'),
--- samples/webappsample/webapp/views.py
+++ samples/webappsample/webapp/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from configcatclient.user import User
 from webapp.apps import WebappConfig
+from django.template import loader
 
 # In the project there is a 'keySampleText' setting with the following rules:
 # 1. If the User's country is Hungary, the value should be 'Dog'
 # 2. If the User's custom property - SubscriptionType - is unlimited, the value should be 'Lion'
 # 3. In other cases there is a percentage rollout configured with 50% 'Falcon' and 50% 'Horse' rules.
 # 4. There is also a default value configured: 'Cat'
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(request=request))
 
 
 # 1. As the passed User's country is Hungary this will return 'Dog'.
@@ -41,7 +47,7 @@ def index4(request):
     return HttpResponse("Hello, world. 'keySampleText' value from ConfigCat: " + str(my_setting_value))
 
 
-# 'myKeyNotExits' setting doesn't exist in the project configuration and the client returns default value ('N/A');
+# 5. 'myKeyNotExits' setting doesn't exist in the project configuration and the client returns default value ('N/A');
 def index5(request):
     my_setting_value = WebappConfig.configcat_client.get_value('myKeyNotExits', 'N/A')
     return HttpResponse("Hello, world. 'keySampleText' value from ConfigCat: " + str(my_setting_value))
