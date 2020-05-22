0771c8bd8972bd10ff3cf15a1b87255a7a0807bd
Tyrone Abdy <tyrone.abdy@clipchamp.com>
2020-05-22 15:42:21 +1000

    add new read write lock that prioritises writers
    This adds a new read write lock to allow writers to
    have priority if acquiring the lock so that they
    don't get starved by continued readers adding to the queue.

--- configcatclient/readwritelock.py
+++ configcatclient/readwritelock.py
@@ -30,3 +30,75 @@ class ReadWriteLock(object):
 
     def release_write(self):
         self.__exclude.release()
+
+
+class RWLock(object):
+    """Synchronization object used in a solution of so-called second 
+    readers-writers problem. In this problem, many readers can simultaneously 
+    access a share, and a writer has an exclusive access to this share.
+    Additionally, the following constraints should be met: 
+    1) no reader should be kept waiting if the share is currently opened for 
+        reading unless a writer is also waiting for the share, 
+    2) no writer should be kept waiting for the share longer than absolutely 
+        necessary. 
+
+    The implementation is based on [1, secs. 4.2.2, 4.2.6, 4.2.7] 
+    with a modification -- adding an additional lock (C{self.__readers_queue})
+    -- in accordance with [2].
+
+    Sources:
+    [1] A.B. Downey: "The little book of semaphores", Version 2.1.5, 2008
+    [2] P.J. Courtois, F. Heymans, D.L. Parnas:
+        "Concurrent Control with 'Readers' and 'Writers'", 
+        Communications of the ACM, 1971 (via [3])
+    [3] http://en.wikipedia.org/wiki/Readers-writers_problem
+    """
+
+    def __init__(self):
+        self.__read_switch = _LightSwitch()
+        self.__write_switch = _LightSwitch()
+        self.__no_readers = threading.Lock()
+        self.__no_writers = threading.Lock()
+        self.__readers_queue = threading.Lock()
+        """A lock giving an even higher priority to the writer in certain
+        cases (see [2] for a discussion)"""
+
+    def acquire_read(self):
+        self.__readers_queue.acquire()
+        self.__no_readers.acquire()
+        self.__read_switch.acquire(self.__no_writers)
+        self.__no_readers.release()
+        self.__readers_queue.release()
+
+    def release_read(self):
+        self.__read_switch.release(self.__no_writers)
+
+    def acquire_write(self):
+        self.__write_switch.acquire(self.__no_readers)
+        self.__no_writers.acquire()
+
+    def release_write(self):
+        self.__no_writers.release()
+        self.__write_switch.release(self.__no_readers)
+
+
+class _LightSwitch(object):
+    """An auxiliary "light switch"-like object. The first thread turns on the 
+    "switch", the last one turns it off (see [1, sec. 4.2.2] for details)."""
+    def __init__(self):
+        self.__counter = 0
+        self.__mutex = threading.Lock()
+
+    def acquire(self, lock):
+        self.__mutex.acquire()
+        self.__counter += 1
+        if self.__counter == 1:
+            lock.acquire()
+        self.__mutex.release()
+
+    def release(self, lock):
+        self.__mutex.acquire()
+        self.__counter -= 1
+        if self.__counter == 0:
+            lock.release()
+        self.__mutex.release()
