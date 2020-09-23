c76327b7eb2100a3062e69b0e750994729d2b265
Lajos <lajos88.szoke@gmail.com>
2020-09-23 15:59:03 +0200

    5.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
     return [line for line in lines if line]
 
 
-configcatclient_version = '4.1.0'
+configcatclient_version = '5.0.0'
 
 requirements = parse_requirements('requirements.txt')
 
