d6b89cd433f5e320988e4e5a252ea0e8cab1b617
Lajos <lajos88.szoke@gmail.com>
2020-09-30 13:56:42 +0200

    comment fix

--- configcatclient/datagovernance.py
+++ configcatclient/datagovernance.py
@@ -3,7 +3,8 @@ from enum import IntEnum
 
 class DataGovernance(IntEnum):
     """
-    Restrict the location of your feature flag and setting data within the ConfigCat CDN. \n
+    Control the location of the config.json files containing your feature flags
+    and settings within the ConfigCat CDN. \n
     Global: Your data will be published to all ConfigCat CDN nodes to guarantee lowest response times. \n
     EuOnly: Your data will be published to CDN nodes only in the EU.
     """
