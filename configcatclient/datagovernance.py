35349d290be8fedc38d7cf63f6d2e95ddb5bf6a6
Lajos <lajos88.szoke@gmail.com>
2020-09-30 14:05:06 +0200

    comments

--- configcatclient/datagovernance.py
+++ configcatclient/datagovernance.py
@@ -5,8 +5,8 @@ class DataGovernance(IntEnum):
     """
     Control the location of the config.json files containing your feature flags
     and settings within the ConfigCat CDN. \n
-    Global: Your data will be published to all ConfigCat CDN nodes to guarantee lowest response times. \n
-    EuOnly: Your data will be published to CDN nodes only in the EU.
+    Global: If your data is published to all global ConfigCat CDN nodes. \n
+    EuOnly: If your data is published to CDN nodes only in the EU.
     """
     Global = 0
     EuOnly = 1
