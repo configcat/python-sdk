ffbc1ef5e803ddb4d06657e74b55ee753fc5e476
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-03-01 10:31:38 +0100

    oneline logging in RolloutEvaluator

--- configcatclient/rolloutevaluator.py
+++ configcatclient/rolloutevaluator.py
@@ -33,8 +33,6 @@ class RolloutEvaluator(object):
     ]
 
     def evaluate(self, key, user, default_value, default_variation_id, config):
-        log.info('Evaluating get_value(\'%s\').' % key)
-
         feature_flags = config.get(FEATURE_FLAGS, None)
         if feature_flags is None:
             log.error('Evaluating get_value(\'%s\') failed. Value not found for key \'%s\' '
@@ -69,126 +67,135 @@ class RolloutEvaluator(object):
             log.info('Returning [%s]' % str(return_value))
             return return_value, return_variation_id
 
-        log.info('User object:\n%s' % str(user))
+        log_entries = ['Evaluating get_value(\'%s\').' % key, 'User object:\n%s' % str(user)]
 
-        # Evaluate targeting rules
-        for rollout_rule in rollout_rules:
-            comparison_attribute = rollout_rule.get(COMPARISON_ATTRIBUTE)
-            comparison_value = rollout_rule.get(COMPARISON_VALUE)
-            comparator = rollout_rule.get(COMPARATOR)
+        try:
+            # Evaluate targeting rules
+            for rollout_rule in rollout_rules:
+                comparison_attribute = rollout_rule.get(COMPARISON_ATTRIBUTE)
+                comparison_value = rollout_rule.get(COMPARISON_VALUE)
+                comparator = rollout_rule.get(COMPARATOR)
 
-            user_value = user.get_attribute(comparison_attribute)
-            if user_value is None or not user_value:
-                log.info(self._format_no_match_rule(comparison_attribute, user_value, comparator, comparison_value))
-                continue
-
-            value = rollout_rule.get(VALUE)
-            variation_id = rollout_rule.get(VARIATION_ID, default_variation_id)
-
-            # IS ONE OF
-            if comparator == 0:
-                if str(user_value) in [x.strip() for x in str(comparison_value).split(',')]:
-                    log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                     comparison_value, value))
-                    return value, variation_id
-            # IS NOT ONE OF
-            elif comparator == 1:
-                if str(user_value) not in [x.strip() for x in str(comparison_value).split(',')]:
-                    log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                     comparison_value, value))
-                    return value, variation_id
-            # CONTAINS
-            elif comparator == 2:
-                if str(user_value).__contains__(str(comparison_value)):
-                    log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                     comparison_value, value))
-                    return value, variation_id
-            # DOES NOT CONTAIN
-            elif comparator == 3:
-                if not str(user_value).__contains__(str(comparison_value)):
-                    log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                     comparison_value, value))
-                    return value, variation_id
-
-            # IS ONE OF, IS NOT ONE OF (Semantic version)
-            elif 4 <= comparator <= 5:
-                try:
-                    match = False
-                    for x in filter(None, [x.strip() for x in str(comparison_value).split(',')]):
-                        match = semver.match(str(user_value).strip(), '==' + x) or match
-                    if (match and comparator == 4) or (not match and comparator == 5):
-                        log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                         comparison_value, value))
-                        return value, variation_id
-                except ValueError as e:
-                    log.warning(self._format_validation_error_rule(comparison_attribute, user_value, comparator,
-                                                                   comparison_value, str(e)))
+                user_value = user.get_attribute(comparison_attribute)
+                if user_value is None or not user_value:
+                    log_entries.append(self._format_no_match_rule(comparison_attribute, user_value, comparator, comparison_value))
                     continue
 
-            # LESS THAN, LESS THAN OR EQUALS TO, GREATER THAN, GREATER THAN OR EQUALS TO (Semantic version)
-            elif 6 <= comparator <= 9:
-                try:
-                    if semver.match(str(user_value).strip(),
-                                    self.SEMANTIC_VERSION_COMPARATORS[comparator - 6] + str(comparison_value).strip()):
-                        log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                         comparison_value, value))
+                value = rollout_rule.get(VALUE)
+                variation_id = rollout_rule.get(VARIATION_ID, default_variation_id)
+
+                # IS ONE OF
+                if comparator == 0:
+                    if str(user_value) in [x.strip() for x in str(comparison_value).split(',')]:
+                        log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                   comparison_value, value))
                         return value, variation_id
-                except ValueError as e:
-                    log.warning(self._format_validation_error_rule(comparison_attribute, user_value, comparator,
-                                                                   comparison_value, str(e)))
-                    continue
-            elif 10 <= comparator <= 15:
-                try:
-                    user_value_float = float(str(user_value).replace(",", "."))
-                    comparison_value_float = float(str(comparison_value).replace(",", "."))
-
-                    if (comparator == 10 and user_value_float == comparison_value_float) \
-                            or (comparator == 11 and user_value_float != comparison_value_float) \
-                            or (comparator == 12 and user_value_float < comparison_value_float) \
-                            or (comparator == 13 and user_value_float <= comparison_value_float) \
-                            or (comparator == 14 and user_value_float > comparison_value_float) \
-                            or (comparator == 15 and user_value_float >= comparison_value_float):
-                        log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                         comparison_value, value))
+                # IS NOT ONE OF
+                elif comparator == 1:
+                    if str(user_value) not in [x.strip() for x in str(comparison_value).split(',')]:
+                        log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                   comparison_value, value))
                         return value, variation_id
-                except Exception as e:
-                    log.warning(self._format_validation_error_rule(comparison_attribute, user_value, comparator,
-                                                                   comparison_value, str(e)))
-                    continue
-            # IS ONE OF (Sensitive)
-            elif comparator == 16:
-                if str(hashlib.sha1(user_value.encode('utf8')).hexdigest()) in [x.strip() for x in str(comparison_value).split(',')]:
-                    log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                     comparison_value, value))
-                    return value, variation_id
-            # IS NOT ONE OF (Sensitive)
-            elif comparator == 17:
-                if str(hashlib.sha1(user_value.encode('utf8')).hexdigest()) not in [x.strip() for x in str(comparison_value).split(',')]:
-                    log.info(self._format_match_rule(comparison_attribute, user_value, comparator,
-                                                     comparison_value, value))
-                    return value, variation_id
+                # CONTAINS
+                elif comparator == 2:
+                    if str(user_value).__contains__(str(comparison_value)):
+                        log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                   comparison_value, value))
+                        return value, variation_id
+                # DOES NOT CONTAIN
+                elif comparator == 3:
+                    if not str(user_value).__contains__(str(comparison_value)):
+                        log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                   comparison_value, value))
+                        return value, variation_id
 
-            log.info(self._format_no_match_rule(comparison_attribute, user_value, comparator, comparison_value))
+                # IS ONE OF, IS NOT ONE OF (Semantic version)
+                elif 4 <= comparator <= 5:
+                    try:
+                        match = False
+                        for x in filter(None, [x.strip() for x in str(comparison_value).split(',')]):
+                            match = semver.match(str(user_value).strip(), '==' + x) or match
+                        if (match and comparator == 4) or (not match and comparator == 5):
+                            log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                       comparison_value, value))
+                            return value, variation_id
+                    except ValueError as e:
+                        message = self._format_validation_error_rule(comparison_attribute, user_value, comparator,
+                                                                     comparison_value, str(e))
+                        log.warning(message)
+                        log_entries.append(message)
+                        continue
 
-        # Evaluate variations
-        if len(rollout_percentage_items) > 0:
-            user_key = user.get_identifier()
-            hash_candidate = ('%s%s' % (key, user_key)).encode('utf-8')
-            hash_val = int(hashlib.sha1(hash_candidate).hexdigest()[:7], 16) % 100
+                # LESS THAN, LESS THAN OR EQUALS TO, GREATER THAN, GREATER THAN OR EQUALS TO (Semantic version)
+                elif 6 <= comparator <= 9:
+                    try:
+                        if semver.match(str(user_value).strip(),
+                                        self.SEMANTIC_VERSION_COMPARATORS[comparator - 6] + str(comparison_value).strip()):
+                            log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                       comparison_value, value))
+                            return value, variation_id
+                    except ValueError as e:
+                        message = self._format_validation_error_rule(comparison_attribute, user_value, comparator,
+                                                                     comparison_value, str(e))
+                        log.warning(message)
+                        log_entries.append(message)
+                        continue
+                elif 10 <= comparator <= 15:
+                    try:
+                        user_value_float = float(str(user_value).replace(",", "."))
+                        comparison_value_float = float(str(comparison_value).replace(",", "."))
 
-            bucket = 0
-            for rollout_percentage_item in rollout_percentage_items or []:
-                bucket += rollout_percentage_item.get(PERCENTAGE, 0)
-                if hash_val < bucket:
-                    percentage_value = rollout_percentage_item.get(VALUE)
-                    variation_id = rollout_percentage_item.get(VARIATION_ID, default_variation_id)
-                    log.info('Evaluating %% options. Returning %s' % percentage_value)
-                    return percentage_value, variation_id
+                        if (comparator == 10 and user_value_float == comparison_value_float) \
+                                or (comparator == 11 and user_value_float != comparison_value_float) \
+                                or (comparator == 12 and user_value_float < comparison_value_float) \
+                                or (comparator == 13 and user_value_float <= comparison_value_float) \
+                                or (comparator == 14 and user_value_float > comparison_value_float) \
+                                or (comparator == 15 and user_value_float >= comparison_value_float):
+                            log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                       comparison_value, value))
+                            return value, variation_id
+                    except Exception as e:
+                        message = self._format_validation_error_rule(comparison_attribute, user_value, comparator,
+                                                                     comparison_value, str(e))
+                        log.warning(message)
+                        log_entries.append(message)
+                        continue
+                # IS ONE OF (Sensitive)
+                elif comparator == 16:
+                    if str(hashlib.sha1(user_value.encode('utf8')).hexdigest()) in [x.strip() for x in str(comparison_value).split(',')]:
+                        log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                   comparison_value, value))
+                        return value, variation_id
+                # IS NOT ONE OF (Sensitive)
+                elif comparator == 17:
+                    if str(hashlib.sha1(user_value.encode('utf8')).hexdigest()) not in [x.strip() for x in str(comparison_value).split(',')]:
+                        log_entries.append(self._format_match_rule(comparison_attribute, user_value, comparator,
+                                                                   comparison_value, value))
+                        return value, variation_id
 
-        return_value = setting_descriptor.get(VALUE, default_value)
-        return_variation_id = setting_descriptor.get(VARIATION_ID, default_variation_id)
-        log.info('Returning %s' % return_value)
-        return return_value, return_variation_id
+                log_entries.append(self._format_no_match_rule(comparison_attribute, user_value, comparator, comparison_value))
+
+            # Evaluate variations
+            if len(rollout_percentage_items) > 0:
+                user_key = user.get_identifier()
+                hash_candidate = ('%s%s' % (key, user_key)).encode('utf-8')
+                hash_val = int(hashlib.sha1(hash_candidate).hexdigest()[:7], 16) % 100
+
+                bucket = 0
+                for rollout_percentage_item in rollout_percentage_items or []:
+                    bucket += rollout_percentage_item.get(PERCENTAGE, 0)
+                    if hash_val < bucket:
+                        percentage_value = rollout_percentage_item.get(VALUE)
+                        variation_id = rollout_percentage_item.get(VARIATION_ID, default_variation_id)
+                        log_entries.append('Evaluating %% options. Returning %s' % percentage_value)
+                        return percentage_value, variation_id
+
+            return_value = setting_descriptor.get(VALUE, default_value)
+            return_variation_id = setting_descriptor.get(VARIATION_ID, default_variation_id)
+            log_entries.append('Returning %s' % return_value)
+            return return_value, return_variation_id
+        finally:
+            log.info('\n'.join(log_entries))
 
     def _format_match_rule(self, comparison_attribute, user_value, comparator, comparison_value, value):
         return 'Evaluating rule: [%s:%s] [%s] [%s] => match, returning: %s' \
