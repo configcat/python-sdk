55d50c699749c0a789015d1d4dab74cec6d60e1a
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-12-16 23:00:34 +0100

    user fix

--- configcatclienttests/test_user.py
+++ configcatclienttests/test_user.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from configcatclient.user import User
 
 
@@ -10,13 +11,35 @@ class UserTests(unittest.TestCase):
         self.assertEqual('', u2.get_identifier())
 
     def test_attribute_case_sensitivity(self):
+        user_id = 'id'
         email = 'test@test.com'
         country = 'country'
-        user = User('user_id', email=email, country=country)
-        self.assertEqual(email, user.get_attribute("Email"))
-        self.assertIsNone(user.get_attribute("EMAIL"))
-        self.assertIsNone(user.get_attribute("email"))
+        custom = {'custom': 'test'}
+        user = User(identifier=user_id, email=email, country=country, custom=custom)
 
-        self.assertEqual(country, user.get_attribute("Country"))
-        self.assertIsNone(user.get_attribute("COUNTRY"))
-        self.assertIsNone(user.get_attribute("country"))
+        self.assertEqual(user_id, user.get_identifier())
+
+        self.assertEqual(email, user.get_attribute('Email'))
+        self.assertIsNone(user.get_attribute('EMAIL'))
+        self.assertIsNone(user.get_attribute('email'))
+
+        self.assertEqual(country, user.get_attribute('Country'))
+        self.assertIsNone(user.get_attribute('COUNTRY'))
+        self.assertIsNone(user.get_attribute('country'))
+
+        self.assertEqual('test', user.get_attribute('custom'))
+        self.assertIsNone(user.get_attribute('non-existing'))
+
+    def test_to_str(self):
+        user_id = 'id'
+        email = 'test@test.com'
+        country = 'country'
+        custom = {'custom': 'test'}
+        user = User(identifier=user_id, email=email, country=country, custom=custom)
+
+        user_json = json.loads(str(user))
+
+        self.assertEqual(user_id, user_json['Identifier'])
+        self.assertEqual(email, user_json['Email'])
+        self.assertEqual(country, user_json['Country'])
+        self.assertEqual(custom, user_json['Custom'])
