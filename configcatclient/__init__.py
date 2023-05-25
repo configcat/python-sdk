ae0a0921d7ccc581340c3fc04d6cf065b847ed1b
kp-cat <52385411+kp-cat@users.noreply.github.com>
2023-05-25 20:50:13 +0200

    lint fixes

--- configcatclient/__init__.py
+++ configcatclient/__init__.py
@@ -1,8 +1,8 @@
 from .configcatclient import ConfigCatClient
 from .interfaces import ConfigCatClientException  # noqa: F401
-from .datagovernance import DataGovernance
-from .configcatoptions import ConfigCatOptions
-from .pollingmode import PollingMode
+from .datagovernance import DataGovernance  # noqa: F401
+from .configcatoptions import ConfigCatOptions  # noqa: F401
+from .pollingmode import PollingMode  # noqa: F401
 
 
 def get(sdk_key, options=None):
