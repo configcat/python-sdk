41bf5d06bc820055faa73e8987c4688c5328f3fc
kp-cat <52385411+kp-cat@users.noreply.github.com>
2023-11-27 12:15:47 +0100

    bump version 9.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
     return [line for line in lines if line]
 
 
-configcatclient_version = '8.0.0'
+configcatclient_version = '9.0.0'
 
 requirements = parse_requirements('requirements.txt')
 
