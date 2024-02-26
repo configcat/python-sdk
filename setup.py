3591d337ab7d7c2185b18fee223fbe98cfb2132d
kp-cat <52385411+kp-cat@users.noreply.github.com>
2024-02-27 00:04:46 +0100

    version 9.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
     return [line for line in lines if line]
 
 
-configcatclient_version = '9.0.2'
+configcatclient_version = '9.0.3'
 
 requirements = parse_requirements('requirements.txt')
 
