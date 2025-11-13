e37ea59190949665cf9e9d5ddcbe7e4edbf1c768
kp-cat <52385411+kp-cat@users.noreply.github.com>
2025-11-13 11:41:01 +0100

    fix concurrency_process test: ConfigCat client cannot be  pass to other process: reason="TypeError: can't pickle _thread.lock objects". To avoid this, we pass only the sdk_key and get the singleton instance in the process.

--- configcatclienttests/test_concurrency.py
+++ configcatclienttests/test_concurrency.py
@@ -12,24 +12,29 @@ from configcatclient.user import User
 logging.basicConfig(level=logging.WARN)
 
 
-def _manual_force_refresh(client, repeat=10, delay=0.1):
-    for i in range(repeat):
+def _manual_force_refresh(sdk_key, repeat=10, delay=0.1):
+    client = configcatclient.get(sdk_key)
+    for _ in range(repeat):
         client.force_refresh()
         sleep(delay)
 
 
 class ConcurrencyTests(unittest.TestCase):
-    @pytest.mark.skipif(sys.platform == 'win32' or sys.platform == 'darwin', reason="TypeError: can't pickle _thread.lock objects")
+
     def test_concurrency_process(self):
-        client = configcatclient.get('PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A')
+        sdk_key = "PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A"
+        client = configcatclient.get(sdk_key)
         value = client.get_value('keySampleText', False, User('key'))
         print("'keySampleText' value from ConfigCat: " + str(value))
 
-        p1 = multiprocessing.Process(target=_manual_force_refresh, args=(client,))
-        p2 = multiprocessing.Process(target=_manual_force_refresh, args=(client,))
+        p1 = multiprocessing.Process(target=_manual_force_refresh, args=(sdk_key,))
+        p2 = multiprocessing.Process(target=_manual_force_refresh, args=(sdk_key,))
         p1.start()
         p2.start()
         p1.join()
         p2.join()
 
         client.close()
+
+        self.assertEqual(p1.exitcode, 0, f"Process {p1.pid} exited with code {p1.exitcode}")
+        self.assertEqual(p2.exitcode, 0, f"Process {p2.pid} exited with code {p2.exitcode}")
