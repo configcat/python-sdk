dc6b1d2c3c156a5faefdde48c81d70d49091d846
kp-cat <52385411+kp-cat@users.noreply.github.com>
2025-11-14 14:45:17 +0100

    fix test_fetch_call_count

--- configcatclienttests/test_autopollingcachepolicy.py
+++ configcatclienttests/test_autopollingcachepolicy.py
@@ -69,7 +69,7 @@ class AutoPollingCachePolicyTests(unittest.TestCase):
         cache_policy = ConfigService('', PollingMode.auto_poll(poll_interval_seconds=2,
                                                                max_init_wait_time_seconds=1),
                                      Hooks(), config_fetcher, log, config_cache, False)
-        time.sleep(3)
+        time.sleep(3.1)
         self.assertEqual(config_fetcher.get_call_count, 2)
         config, _ = cache_policy.get_config()
         settings = config.get(FEATURE_FLAGS)
